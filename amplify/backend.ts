fb8d14fb10c1f88aa3d8c0b6afe5e6be3f0e1dc3
Sofia Escobedo <63127299+yokharian@users.noreply.github.com>
2025-12-03 14:56:30 -0600

    feature: define auth resource

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,13 +1,16 @@
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import { defineBackend } from '@aws-amplify/backend';
-import { storage } from './storage/resource';
+import {defineBackend} from '@aws-amplify/backend';
+import {auth} from './auth/resource';
+import {storage} from './storage/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
+  auth,
   storage
 });
+
 
 
 /**
