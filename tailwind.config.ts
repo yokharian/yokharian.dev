2438d5bd3701cb0fb570631987135fe4246ba275
Sofia Escobedo <63127299+yokharian@users.noreply.github.com>
2025-12-24 13:17:36 -0600

    removed taskmaster

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,6 @@ const config: Config = {
     "./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}",
     "!./node_modules/**/*",
     "!./dist/**/*",
-    "!./.taskmaster/**/*",
     "!./UI_UX_GUIDE/**/*",
   ],
   theme: {
